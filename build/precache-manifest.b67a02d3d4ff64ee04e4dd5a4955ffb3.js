c4e0b78f9cab6b9b892ea4f6f982579f0df4e3a2
HatAndBread <61222965+HatAndBread@users.noreply.github.com>
2020-09-25 09:58:24 +0900

    new build

---
+++ build/precache-manifest.b67a02d3d4ff64ee04e4dd5a4955ffb3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1caaff0538f88fb3ed3d802621b8b7bf",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4bf5941869448415d14c",
+    "url": "/static/css/main.e2510e74.chunk.css"
+  },
+  {
+    "revision": "ee830a88ed8463b49da6",
+    "url": "/static/js/2.f4d8dfcb.chunk.js"
+  },
+  {
+    "revision": "f65b45f562155f0fa2653b346e3552a6",
+    "url": "/static/js/2.f4d8dfcb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4bf5941869448415d14c",
+    "url": "/static/js/main.e542e98f.chunk.js"
+  },
+  {
+    "revision": "68fb5d8584ff8ce13542",
+    "url": "/static/js/runtime-main.b2ddd238.js"
+  }
+]);
